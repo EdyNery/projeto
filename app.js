6a6b6c67db4b8bbb214f4abeea746f0eecc06f7b
EdyNery <edivaldoneryfilho@gmail.com>
2024-07-11 22:11:23 -0300

    frangao

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ html.classList.toggle("light")
 const img = document.querySelector("#profile img")
 
 if(html.classList.contains("light")){
-  img.setAttribute("src", "./assets/avatar.png")
+  img.setAttribute("src", "./assets/frangao.jpg")
 } else{
     img.setAttribute("src", "./assets/Tic.jpg")
 }
